06c86feefb2899ac9d19343d2eb3e8ac5a42a079
David Meir-Levy <david@davidlevy.co.il>
2025-04-09 11:47:18 +0300

    QELOS UI Library: Initial implementation with manifest modification build step

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import QelosSDK from '@qelos/sdk';
+import HelloQelos from './components/HelloQelos.vue';
+
+const sdk = new QelosSDK({
+  appUrl: 'https://api.qelos.io',
+  fetch: globalThis.fetch.bind(globalThis),
+});
+
+const manifestURL = location.origin + '/play-manifest.json';
+</script>
+
+<template>
+  <h1>Demo App</h1> 
+  <p>
+    In order to use this library inside a Qelos app, copy this manifest URL and create a new plugin from it:
+    <el-input type="text" :value="manifestURL" readonly />
+  </p>
+  <HelloQelos :sdk="sdk" />
+</template>
