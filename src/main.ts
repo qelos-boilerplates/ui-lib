4c69aa1360de8668b616025654aecb3eaadba1f4
David Meir-Levy <david@davidlevy.co.il>
2025-05-08 10:37:44 +0300

    fix manifest url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import './assets/main.css';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import pubsub from './utils/pubsub';
-
+import { createRouter, createWebHistory } from 'vue-router';
 
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
@@ -21,5 +21,9 @@ const app = createApp(App);
 app.config.globalProperties.$pubsub = pubsub;
 
 app.use(ElementPlus);
+app.use(createRouter({
+    history: createWebHistory(),
+    routes: [ { path: '/', component: App } ],
+}));
 
 app.mount('#app');
